723b2015aefbf975bab506196566ae6252697b71
Wiktor Kania <roller47@wp.pl>
2020-05-11 08:57:44 +0200

    Automated makefile commit

--- Socket_proxy/server.py
+++ Socket_proxy/server.py
@@ -1,11 +1,21 @@
 import asyncio
 import websockets
 import json
+import http.server
+import socketserver
+import threading
 
 port = 3000
 host = 'localhost'
 handlers = {}
 ws = None
+
+def start_http(arg):
+  port = 3080
+  Handler = http.server.SimpleHTTPRequestHandler
+  with socketserver.TCPServer(("", port), Handler) as httpd:
+    print("HTTP server started at port", port)
+    httpd.serve_forever()
 
 def on(event, callback):
   handlers[event] = callback
@@ -18,7 +28,7 @@ async def send_pong(data):
 
 on('ping', send_pong)
 
-async def server(websocket, path):
+async def ws_server(websocket, path):
   global ws
   ws = websocket
   ready_event = {'event': 'ready', 'data': 'Hello'}
@@ -33,7 +43,9 @@ async def server(websocket, path):
     if msg['event'] in handlers:
       await handlers[msg['event']](msg['data'])
 
-start_server = websockets.serve(server, host, 3000)
+threading._start_new_thread(start_http, (None, ))
+
+start_server = websockets.serve(ws_server, host, 3000)
 print(f"Socket server started on {host}:{port}")
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
